9661cf80ecd4d4e71590dad3567f964a5358deb8
Tongs <62232130+thetongs@users.noreply.github.com>
2022-03-16 17:31:14 +0530

    Create 6. write_a_function.py

---
+++ 6. write_a_function.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    if(year >= 1900 and year <= 10 ** 5 ):
+        if(year % 400 == 0 or year % 4 == 0 and year % 100 != 0 ):
+            leap = True
+        
+        return leap
+
+year = int(input())
+print(is_leap(year))
