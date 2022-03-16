c588d3dd9d03e6724561f8f29945d7f452c09f05
Tongs <62232130+thetongs@users.noreply.github.com>
2022-03-16 17:25:26 +0530

    Delete 7_Print_Function.py

--- 7_Print_Function.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    if(n >= 1 and n <= 150):
-        numbers = [*range(1, n+1)]
-    
-        final = ''.join(str(n) for n in numbers)
-        print(final)
-        
